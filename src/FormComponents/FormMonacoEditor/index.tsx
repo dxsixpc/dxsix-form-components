fc6e4c86d9dbf513436945569f9030ac6d0e7ac6
dxsixpc <1071667708@qq.com>
2021-11-19 10:44:17 +0800

    update

--- src/FormComponents/FormMonacoEditor/index.tsx
+++ src/FormComponents/FormMonacoEditor/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Editor from '@monaco-editor/react';
-import type { EditorProps } from '@monaco-editor/react';
+import type { EditorProps, OnChange } from '@monaco-editor/react';
+import { useDebounceFn } from 'ahooks';
 import { Wrapper } from './Styled';
 
 const FormMonacoEditor: React.FC<EditorProps> = (props) => {
@@ -10,17 +11,27 @@ const FormMonacoEditor: React.FC<EditorProps> = (props) => {
     height = '500px',
     defaultLanguage = 'javascript',
     theme = 'vs-dark',
+    onChange,
     ...rest
   } = props;
 
+  // onChange封装
+  const onEditorChange: OnChange = (editorValue, ev) => {
+    if (onChange) onChange(editorValue, ev);
+  };
+
+  // 防抖操作
+  const { run } = useDebounceFn(onChange || onEditorChange, { wait: 200 });
+
   return (
-    <Wrapper className='MonacoEditor'>
+    <Wrapper className='MonacoEditorWidget'>
       <Editor
         width={width}
         height={height}
         defaultLanguage={defaultLanguage}
         theme={theme}
         defaultValue={value || ''}
+        onChange={run}
         {...rest}
       />
     </Wrapper>
